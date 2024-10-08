88276ee2afe60d0479bc5783314849a0817bed49
sydne <sydne@10.220.29.32>
2024-10-08 12:44:03 -0400

    add variables and scanner

--- src/edu/wit/cs/comp1000/PA2a.java
+++ src/edu/wit/cs/comp1000/PA2a.java
@@ -1,11 +1,22 @@
 package edu.wit.cs.comp1000;
 
-// TODO: document this class
+import java.util.Scanner;
 public class PA2a {
 
 	// TODO: document this function
 	public static void main(String[] args) {
-		// TODO: write your code here
-	}
+		 
+		Scanner input = new Scanner(System.in);
+		 	
+		int num1 = input.nextInt();
+		int num2 = input.nextInt();
+		int num3 = input.nextInt();
+		int num4 = input.nextInt();
+		int num5 = input.nextInt();
+			}
+		}
+	
+		
+	
 
-}
+
