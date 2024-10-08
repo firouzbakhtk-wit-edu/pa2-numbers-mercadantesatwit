e427c92fa315555898a0154d7dc94728b94c4456
sydne <sydne@10.220.29.32>
2024-10-08 12:52:01 -0400

    added num1 conditions

--- src/edu/wit/cs/comp1000/PA2a.java
+++ src/edu/wit/cs/comp1000/PA2a.java
@@ -13,6 +13,21 @@ public class PA2a {
 		int num3 = input.nextInt();
 		int num4 = input.nextInt();
 		int num5 = input.nextInt();
+		
+		int positiveSum = 0;
+		int nonpositiveSum = 0;
+		int totalSum = num1 + num2 + num3 + num4 + num5;
+		
+		int positiveCount = 0;
+		int nonpositiveCount = 0;
+		
+		if (num1>0) {
+			positiveSum += num1;
+			positiveCount++;
+		}else {
+			nonpositiveSum+= num1;
+			nonpositiveCount++;
+		}
 			}
 		}
 	
