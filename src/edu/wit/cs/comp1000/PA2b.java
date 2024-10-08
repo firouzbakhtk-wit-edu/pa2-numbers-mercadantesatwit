2cef7e9d41df9d986b9b0231d5fa77d2f5995bb9
sydne <sydne@10.220.29.32>
2024-10-08 18:22:46 -0400

    made sure roots are printed in increasing order

--- src/edu/wit/cs/comp1000/PA2b.java
+++ src/edu/wit/cs/comp1000/PA2b.java
@@ -20,8 +20,14 @@ public class PA2b {
 			double root =-b/(2*a);
 			System.out.printf("Root: %.2f%n", root);
 		} else {
-			double root1= (-b -Math.sqrt(discriminant))
-			double root2 = (-b + Math.sqrt(discriminant))		
+			double root1 = (-b - Math.sqrt(discriminant)) / (2*a);
+			double root2 = (-b + Math.sqrt(discriminant)) / (2*a);	
+			if (root1> root2) {
+				double temp = root1;
+				root1 = root2;
+				root2 = temp;
+			}
+			System.out.printf("Roots: %.2f, %.2f%n", root1, root2);
 		}
 	}
 
