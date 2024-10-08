48b04e50ff73cd9f30f8fcbae71ab560643d4a47
sydne <sydne@10.220.29.32>
2024-10-08 13:07:00 -0400

    added if else statements for each number and how to find the average and sum of each and output code and scanner
    Change-Id: Iab4c31657c6699b182f06c6a7d58b3721eef02b2

--- src/edu/wit/cs/comp1000/PA2a.java
+++ src/edu/wit/cs/comp1000/PA2a.java
@@ -7,7 +7,9 @@ public class PA2a {
 	public static void main(String[] args) {
 		 
 		Scanner input = new Scanner(System.in);
-		 	
+		
+		System.out.printf("Enter 5 whole numbers: %n");
+		
 		int num1 = input.nextInt();
 		int num2 = input.nextInt();
 		int num3 = input.nextInt();
@@ -28,6 +30,45 @@ public class PA2a {
 			nonpositiveSum+= num1;
 			nonpositiveCount++;
 		}
+		if (num2>0) {
+			positiveSum += num2;
+			positiveCount++;
+		}else {
+			nonpositiveSum+= num2;
+			nonpositiveCount++;
+		}
+		if (num3>0) {
+			positiveSum += num3;
+			positiveCount++;
+		}else {
+			nonpositiveSum+= num3;
+			nonpositiveCount++;
+		}
+		if (num4>0) {
+			positiveSum += num4;
+			positiveCount++;
+		}else {
+			nonpositiveSum+= num4;
+			nonpositiveCount++;
+		}
+		if (num5>0) {
+			positiveSum += num5;
+			positiveCount++;
+		}else {
+			nonpositiveSum+= num5;
+			nonpositiveCount++;
+		}
+		double positiveAverage = (double) positiveSum / positiveCount;
+		double nonpositiveAverage = (double) nonpositiveSum / nonpositiveCount;
+		double totalAverage = (double) totalSum / 5;
+		
+		 System.out.printf("Sum of positive numbers: %d%n", positiveSum);
+	        System.out.printf("Sum of non-positive numbers: %d%n", nonpositiveSum);
+	        System.out.printf("Sum of all numbers: %d%n", totalSum);
+	        System.out.printf("Average of positive numbers: %.2f%n", positiveAverage);
+	        System.out.printf("Average of non-positive numbers: %.2f%n", nonpositiveAverage);
+	        System.out.printf("Average of all numbers: %.2f%n", totalAverage);
+		
 			}
 		}
 	
